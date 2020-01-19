96a08eecee127309c7e26c46fda77c7242dae842
Steve C <stevec@archwayit.co.uk>
2020-01-19 16:00:14 +0000

    Commit of code to support blog post

---
+++ ObjectOrientatedNamedServiceProviderDemo.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace NamedKeyDIDemo
+{
+    public class ObjectOrientatedNamedServiceProviderDemo
+    {
+        [Fact]
+        public void Test_Temperature_Mapper_Directly()
+        {
+            var sut = ObjectOrientatedNamedServiceProvider.Get();
+
+            var converterMapper = sut.GetRequiredService<KelvinConverterMapper>();
+            var mappings = _testCases.Select(tc =>
+                (tc.Input, tc.ExpectedResult, ActualResult: converterMapper(tc.TempScale).ToKelvins(tc.Input)));
+            var tolerance = new Func<decimal, decimal>(d => Math.Round(d, 2, MidpointRounding.ToEven));
+
+            foreach (var (_, expectedResult, actualResult) in mappings)
+            {
+                Assert.Equal(tolerance(expectedResult), tolerance(actualResult));
+            }
+        }
+
+        [Fact]
+        public void Test_Temperature_Mapper_Via_Class()
+        {
+            var sut = ObjectOrientatedNamedServiceProvider.Get();
+
+            var converterMapper = sut.GetRequiredService<TemperatureCalculator>();
+
+            var mappings = _testCases.Select(tc =>
+                (tc.Input, tc.ExpectedResult, ActualResult: converterMapper.GetTemperatureInKelvins(tc.TempScale, tc.Input)));
+            var tolerance = new Func<decimal, decimal>(d => Math.Round(d, 2, MidpointRounding.ToEven));
+
+            foreach (var (_, expectedResult, actualResult) in mappings)
+            {
+                Assert.Equal(tolerance(expectedResult), tolerance(actualResult));
+            }
+        }
+
+        private static Lazy<(string TempScale, decimal Input, decimal ExpectedResult)[]> _testCasesInitializer = new Lazy<(string TempScale, decimal Input, decimal ExpectedResult)[]>(
+            () =>
+                new (string TempScale, decimal Input, decimal ExpectedResult)[]
+                {
+                    ("Centigrade", 0, 273.15M),
+                    ("Centigrade", 100, 373.15M),
+                    ("Fahrenheit", 0, 255.37M),
+                    ("Fahrenheit", 32, 273.15M),
+                    ("Fahrenheit", 100, 310.928M),
+                    ("Rankin", 0, 0),
+                    ("Rankin", 250, 138.89M),
+                    ("Kelvin", 0, 0M),
+                    ("Kelvin", 100.12M, 100.12M),
+                });
+
+        private static (string TempScale, decimal Input, decimal ExpectedResult)[] _testCases = _testCasesInitializer.Value;
+
+        public class TemperatureCalculator
+        {
+            private readonly KelvinConverterMapper _mapperLookup;
+
+            public TemperatureCalculator(KelvinConverterMapper mapperLookup)
+            {
+                _mapperLookup = mapperLookup;
+            }
+
+            public decimal GetTemperatureInKelvins(string scale, decimal value) => _mapperLookup(scale).ToKelvins(value);
+        }
+
+        public delegate IKelvinMapper KelvinConverterMapper(string key);
+
+        public interface IKelvinMapper
+        {
+            decimal ToKelvins(decimal value);
+        }
+
+        private static class ObjectOrientatedNamedServiceProvider
+        {
+            private static readonly Lazy<IServiceProvider> Instance = new Lazy<IServiceProvider>(GetServiceProvider);
+
+            public static IServiceProvider Get() => Instance.Value;
+
+            private static IServiceProvider GetServiceProvider()
+            {
+                var services = new ServiceCollection();
+                services.AddSingleton<CentigradeToKelvinMapper>();
+                services.AddSingleton<FahrenheitToKelvinMapper>();
+                services.AddSingleton<RankineToKelvinMapper>();
+                services.AddSingleton<KelvinToKelvinMapper>();
+
+                services.AddSingleton<KelvinConverterMapper>(provider => key =>
+                {
+                    switch ((string.IsNullOrEmpty(key) ? " " : key).ToUpper()[0])
+                    {
+                        case 'C':
+                            return provider.GetRequiredService<CentigradeToKelvinMapper>();
+
+                        case 'F':
+                            return provider.GetRequiredService<FahrenheitToKelvinMapper>();
+
+                        case 'R':
+                            return provider.GetRequiredService<RankineToKelvinMapper>();
+
+                        case 'K':
+                            return provider.GetRequiredService<KelvinToKelvinMapper>();
+
+                        default:
+                            return null;
+                    }
+                });
+
+                services.AddSingleton<TemperatureCalculator>();
+
+                return services.BuildServiceProvider();
+            }
+            
+            private class CentigradeToKelvinMapper : IKelvinMapper
+            {
+                public decimal ToKelvins(decimal value) => value + 273.15M;
+            }
+
+            private class FahrenheitToKelvinMapper : IKelvinMapper
+            {
+                public decimal ToKelvins(decimal value) => (value + 459.67M) * 5 / 9;
+            }
+
+            private class RankineToKelvinMapper : IKelvinMapper
+            {
+                public decimal ToKelvins(decimal value) => value * 5 / 9;
+            }
+
+            private class KelvinToKelvinMapper : IKelvinMapper
+            {
+                public decimal ToKelvins(decimal value) => value;
+            }
+        }
+    }
+}
